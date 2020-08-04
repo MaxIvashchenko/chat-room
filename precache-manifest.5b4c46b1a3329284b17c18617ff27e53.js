614ed2337be0041774573382e6b64f868183d360
Max Ivashchenko <Max@MacBook-Pro-Max.local>
2020-08-04 15:28:55 +0300

    Updates

---
+++ precache-manifest.5b4c46b1a3329284b17c18617ff27e53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd06b79de2b8d1f1f86a64ba36cdee50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b3f959b9ad0b508aae0",
+    "url": "/static/css/main.8c07a1fa.chunk.css"
+  },
+  {
+    "revision": "2375c72c0c7b7512e24b",
+    "url": "/static/js/2.b10e4f28.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.b10e4f28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3f959b9ad0b508aae0",
+    "url": "/static/js/main.dd2880bc.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
