aec94824fdfd0120a2cae05d09e1171df37dfeb3
Max Ivashchenko <Max@MacBook-Pro-Max.local>
2020-08-05 10:36:54 +0300

    Create a React app and publish it to GitHub Pages

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -12,7 +12,6 @@ let socket;
 export default function Chat({ location }) {
     const [name, setName] = useState('')
     const [room, setRoom] = useState('')
-    const [users, setUsers] = useState('')
     const [message, setMessage] = useState('')
     const [messages, setMessages] = useState([])
 
